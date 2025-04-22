95c3872dbe3eed46ad1233d1e51fa055745e0986
Darlei Randel <darley_rom@hotmail.com>
2025-04-21 22:50:07 -0300

    Fixx'

--- terraform/eks/oidc.tf
+++ terraform/eks/oidc.tf
@@ -1,16 +1,13 @@
-# Acessa os dados do cluster já existente
 data "aws_eks_cluster" "cluster" {
   name = "video-processor-eks-cluster"
 }
 
-# Captura o certificado TLS do issuer do OIDC
 data "tls_certificate" "eks" {
-  url = data.aws_eks_cluster.cluster.identity[0].oidc.issuer
+  url = data.aws_eks_cluster.cluster.identity.oidc.issuer
 }
 
-# Cria o OIDC provider no IAM com o issuer e thumbprint do certificado
 resource "aws_iam_openid_connect_provider" "eks" {
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
-  url             = data.aws_eks_cluster.cluster.identity[0].oidc.issuer
+  url             = data.aws_eks_cluster.cluster.identity.oidc.issuer
 }
