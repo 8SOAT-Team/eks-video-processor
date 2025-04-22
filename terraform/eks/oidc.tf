c5a643f55fbaf452a4ec5f527d51aba180666b0f
Darlei Randel <darley_rom@hotmail.com>
2025-04-21 22:57:09 -0300

    Fixx'

--- terraform/eks/oidc.tf
+++ terraform/eks/oidc.tf
@@ -7,3 +7,47 @@ resource "aws_iam_openid_connect_provider" "eks" {
   thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
   url             = "https://oidc.eks.us-east-1.amazonaws.com/id/3B3D9AC7DA39D280541FF3C278EF3B84"
 }
+
+resource "aws_iam_role" "irsa_sqs_role" {
+  name = "notificacao-api-irsa-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.eks.arn
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringEquals = {
+            "oidc.eks.us-east-1.amazonaws.com/id/3B3D9AC7DA39D280541FF3C278EF3B84:sub" = "system:serviceaccount:fast-video:notificacao-api-sa",
+            "oidc.eks.us-east-1.amazonaws.com/id/3B3D9AC7DA39D280541FF3C278EF3B84:aud" = "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# Policy com permissões para consumir SQS
+resource "aws_iam_policy" "sqs_policy" {
+  name = "notificacao-api-sqs-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueAttributes"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
