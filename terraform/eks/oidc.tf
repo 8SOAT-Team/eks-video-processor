5079d87bb1ac39505e20275ad0863b7e1ee2f015
Darlei Randel <darley_rom@hotmail.com>
2025-04-21 22:53:21 -0300

    Fixx'

--- terraform/eks/oidc.tf
+++ terraform/eks/oidc.tf
@@ -1,13 +1,9 @@
-data "aws_eks_cluster" "cluster" {
-  name = "video-processor-eks-cluster"
-}
-
 data "tls_certificate" "eks" {
-  url = data.aws_eks_cluster.cluster.identity.oidc.issuer
+  url = "https://oidc.eks.us-east-1.amazonaws.com/id/3B3D9AC7DA39D280541FF3C278EF3B84"
 }
 
 resource "aws_iam_openid_connect_provider" "eks" {
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
-  url             = data.aws_eks_cluster.cluster.identity.oidc.issuer
+  url             = "https://oidc.eks.us-east-1.amazonaws.com/id/3B3D9AC7DA39D280541FF3C278EF3B84"
 }
