ecb063699ce4917c2fe9fd6f735bcdeb35a130bb
Darlei Randel <68856429+randeldarlei@users.noreply.github.com>
2025-04-24 20:40:38 -0300

    Update cluster.tf

--- terraform/eks/cluster.tf
+++ terraform/eks/cluster.tf
@@ -28,8 +28,8 @@ resource "aws_eks_node_group" "video_processor_node_group" {
   node_role_arn   = aws_iam_role.node_group_role.arn
 
   scaling_config {
-    desired_size = 1
-    max_size     = 2
+    desired_size = 2
+    max_size     = 3
     min_size     = 1
   }
 
